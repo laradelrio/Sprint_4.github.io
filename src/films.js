072235e758b2f299b2f92c920b0384bfc7212571
laradelrio <lara.delrio333@gmail.com>
2023-07-07 13:28:25 +0200

    first functions draft

--- src/films.js
+++ src/films.js
@@ -1,23 +1,51 @@
 // Exercise 1: Get the array of all directors.
-function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
-  return result;
-}
+
+const getAllDirectors = (movies) =>  movies.map(movie => movie.director);
+console.log(getAllDirectors);
+
+    /*function getAllDirectors(movies){
+      return movies.map(movie => movie.director);
+    }
+    */
+
+    /*function getAllDirectors(array) {
+      let result =  ???;
+      console.log("EXERCICE 1 ->", result);
+      return result;
+    }*/
 
 // Exercise 2: Get the films of a certain director
-function getMoviesFromDirector(array, director) {
- 
-}
+const getMoviesFromDirector = (movies,director) => movies.filter(movie => movie.director===director);
+console.log(getMoviesFromDirector);
+    /*function getMoviesFromDirector(movies, director) {
+      return movies.filter(movie => movie.director===director);
+    }*/
+
 
 // Exercise 3: Calculate the average of the films of a given director.
-function moviesAverageOfDirector(array, director) {
-  
+function moviesAverageOfDirector(movies, director) {
+      let filmsFromDirector = getMoviesFromDirector(movies,director);
+      return filmsFromDirector.reduce((averageRate,film) => (averageRate + film.rate),0); 
 }
 
 // Exercise 4:  Alphabetic order by title 
-function orderAlphabetically(array) {
-  
+const orderAlphabetically = movies => movies.sort
+
+function orderAlphabetically(movies) {
+  let alphabeticallyMovies =  movies.sort((a,b) =>{
+    
+    if (a.name < nameB){
+      return -1;
+    }
+    if (a.name > b.name){
+      return 1;
+    }
+    return 0;
+  })
+  let alphabeticallyMovies20 = alphabeticallyMovies.slice(0,20);
+  let alphabeticallyMoviesName20 = alphabeticallyMovies20.map( movie => movie.name);
+  return alphabeticallyMoviesName20;
+
 }
 
 // Exercise 5: Order by year, ascending
